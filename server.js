6997d98113c62762f18f8c6e551e87c18a9f1e6a
danielmoi <daniel@ampersandmoi.com>
2016-02-08 15:54:15 +1100

    Access request object

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ console.log('May the node be with you');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.listen(3000, function() {
+app.use(bodyParser.urlencoded({
+  extended: true
+}
+));
+
+app.listen(3000, function () {
   console.log('listening on 30000');
   console.log(__dirname);
 });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-  
+
 });
 
 app.post('/quotes', (req, res) => {
   console.log('got the POST request to /quotes!');
+  console.log(req.body);
+  // { name: 'NODEMAN', quote: 'hello...' }
 });
