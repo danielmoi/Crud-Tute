cb0f21f034bd8def2f27c8761f9c5a410bfe1a1a
danielmoi <daniel@ampersandmoi.com>
2016-02-08 14:52:49 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log('May the node be with you');
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
