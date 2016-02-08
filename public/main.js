6e2c993897a9e0f32fa2d1f778d1ba22dc398c18
danielmoi <daniel@ampersandmoi.com>
2016-02-08 18:14:43 +1100

    Create fetch request

---
+++ public/main.js
@@ -0,0 +1,22 @@
+// main.js
+
+var update = document.getElementById('update');
+
+update.addEventListener('click', function () {
+  // send PUT request here
+
+  fetch('quotes', // first param = path (we are sending request to /quote, which will be handled on our server
+
+    // 2nd param = optional object with settings
+    {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // converting quote to JSON format
+        'name': 'Darth Vader',
+        'quote': 'I find your lack of faith disturbing.'
+      })
+    });
+});
